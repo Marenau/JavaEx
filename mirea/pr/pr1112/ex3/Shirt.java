29e3c8e7448ec5f265757995d370593323d8344b
Marenau <malin.vadimka@mail.ru>
2022-10-23 19:26:12 +0300

    Add ex3 in pr1112

---
+++ mirea/pr/pr1112/ex3/Shirt.java
@@ -0,0 +1,23 @@
+package ru.mirea.pr.pr1112.ex3;
+
+public class Shirt {
+    String number, name, color, size;
+
+    public Shirt(String str) {
+        String[] strings = str.split(",");
+        number = strings[0];
+        name = strings[1];
+        color = strings[2];
+        size = strings[3];
+    }
+
+    @Override
+    public String toString() {
+        return "Shirt {" +
+                "\n\tnumber='" + number + '\'' +
+                ",\n\tname='" + name + '\'' +
+                ",\n\tcolor='" + color + '\'' +
+                ",\n\tsize='" + size + '\'' +
+                '}';
+    }
+}
