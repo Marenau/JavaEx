02af8403fc633ceb8b3c7fffa9a286addb422e40
Marenau <malin.vadimka@mail.ru>
2022-11-23 22:46:37 +0300

    Fixed + add comments

--- mirea/pr/pr1314/Student.java
+++ mirea/pr/pr1314/Student.java
@@ -9,7 +9,7 @@ public class Student {
     private int IDNumber = 0;
     static int counter = 1;
     private String FIO;
-    private GregorianCalendar date;
+    private GregorianCalendar date;             //дата рождения
 
     public Student() {
         IDNumber = counter++;
@@ -18,45 +18,51 @@ public class Student {
         this.FIO = FIO;
         IDNumber = counter++;
     }
-    public Student(String FIO, String date){
+    public Student(String FIO, String date){    //конструктор с датой рождения
         this.FIO = FIO;
         IDNumber = counter++;
-        setDate(date);
+        setDate(date);                          //установка даты рождения
     }
 
-    public int getIDNumber() { return IDNumber; }
-    public String getFIO() { return FIO; }
-    public String getDate(char typeOutput) {
-        SimpleDateFormat formatForDate;
-        switch (typeOutput) {
-            case 'm':
-                formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+    public String getDate(char typeOutput) {    //метод получения даты
+        SimpleDateFormat formatForDate;         //формат вывода даты
+        switch (typeOutput) {                   //оператор множества выбора
+            case 'm':                                                   //средний формат вывода
+                formatForDate = new SimpleDateFormat("dd.MM.yyyy");     //присвоение формата
                 break;
-            case 'l':
-                formatForDate = new SimpleDateFormat("dd MMMM yyyy");
+            case 'l':                                                   //полный формат вывода
+                formatForDate = new SimpleDateFormat("dd MMMM yyyy");   //присвоение формата
                 break;
-            default:    //короткий по умолчанию
-                formatForDate = new SimpleDateFormat("dd.MM.yy");
+            default:                                                    //короткий формат по умолчанию
+                formatForDate = new SimpleDateFormat("dd.MM.yy");       //присвоение формата
                 break;
         }
 
         return formatForDate.format(date.getTime());
     }
-    public void setFIO(String FIO) { this.FIO = FIO; }
-    public void setDate(String date) {
-        StringTokenizer st = new StringTokenizer(date, ".");
-        int[] arrayOfDate = new int[st.countTokens()];
-        for (int i = 0; i < 3; i++)
-            arrayOfDate[i] = Integer.parseInt(st.nextToken());
-        GregorianCalendar setDate = new GregorianCalendar();
-        setDate.set(arrayOfDate[2], arrayOfDate[1] - 1, arrayOfDate[0]);    //месяца с нуля
+
+    public void setDate(String date) {                                      //метод установки даты рождения
+        StringTokenizer st = new StringTokenizer(date, ".");                //создание разделителя строки
+        int[] arrayOfDate = new int[st.countTokens()];                      //создание массива для разбиения строки
+        for (int i = 0; i < 3; i++)                                         //цикл присвоения
+            arrayOfDate[i] = Integer.parseInt(st.nextToken());              //присвоение элементам массива данных из строки
+        GregorianCalendar setDate = new GregorianCalendar();                //создание текущей даты
+        setDate.set(arrayOfDate[2], arrayOfDate[1] - 1, arrayOfDate[0]);    //установка даты
+        setDate.set(Calendar.HOUR_OF_DAY, 0);                               //присвоение полям времени нулей
+        setDate.set(Calendar.MINUTE, 0);
+        setDate.set(Calendar.SECOND, 0);
+        setDate.set(Calendar.MILLISECOND, 0);
         this.date = setDate;
     }
 
+    public int getIDNumber() { return IDNumber; }
+    public String getFIO() { return FIO; }
+    public void setFIO(String FIO) { this.FIO = FIO; }
+
     @Override
-    public String toString() {
-        return "Студент: " +
+    public String toString() {                      //переопределённый метод toString
+        return "Студент: " +                        //возврат строки
                 "\nФИО: " + FIO +
-                "\nДата рождения: " + getDate('s');
+                "\nДата рождения: " + getDate('s'); //вывод в коротком формате по умолчанию
     }
 }
--- mirea/pr/pr1314/Test.java
+++ mirea/pr/pr1314/Test.java
@@ -1,133 +1,127 @@
 package ru.mirea.pr.pr1314;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.*;
 
 public class Test {
 
-    public static void printEmployee(String surname) {
-        StringBuilder stringBuilder = new StringBuilder(surname);
+    public static void printEmployee(String surname) {              //метод вывода фамилии и даты
+        StringBuilder stringBuilder = new StringBuilder(surname);   //создание объекта класса stringBuilder
 
         //количество миллисекунд с полуночи 1 янаря 1970
-        Date receiveDate = new Date();
+        Date receiveDate = new Date();                              //создание объекта класса Date
         Date handingDate = new Date(System.currentTimeMillis() + 2628000000L);  //столько миллисекунд в месяце (31 день)
 
-        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM");
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM"); //создание объекта класса для форматирования вывода даты
         stringBuilder.append(".\nЗадание получено: ").append(formatForDate.format(receiveDate))
-                .append("\nДата сдачи: ").append(formatForDate.format(handingDate));
+                .append("\nДата сдачи: ").append(formatForDate.format(handingDate));    //добавление в строку
 
-        System.out.println(stringBuilder);
+        System.out.println(stringBuilder);  //вывод строки
     }
 
-    public static void equalsDate() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите дату в формате 'дд.мм.гггг':");
-        String date = in.nextLine();
-        StringTokenizer st = new StringTokenizer(date, ".");    //разделение через точку
-        int[] arrayOfDate = new int[st.countTokens()];
-        for (int i = 0; i < 3; i++)
-            arrayOfDate[i] = Integer.parseInt(st.nextToken());
+    public static void equalsDate() {                                   //меторд сравнения дат
+        Scanner in = new Scanner(System.in);                            //подключение считывания с консоли
+        System.out.println("Введите дату в формате 'дд.мм.гггг':");     //вывод о необходимом вводе
+        String date = in.nextLine();                                    //считывание из консоли
+        StringTokenizer st = new StringTokenizer(date, ".");            //разделение через точку
+        int[] arrayOfDate = new int[st.countTokens()];                  //разбиение введённой строки
+        for (int i = 0; i < 3; i++)                                     //цикл присвоения
+            arrayOfDate[i] = Integer.parseInt(st.nextToken());          //присвоение элементам массива данных пользователя
 
-        Calendar inputDate = new GregorianCalendar();
-        inputDate.set(arrayOfDate[2], arrayOfDate[1] - 1, arrayOfDate[0]);  //месяца с нуля ыыы
-
-        Calendar currentDate = new GregorianCalendar();
-        if (currentDate.after(inputDate))
-            System.out.println("Ваша дата позже текущей.");
-        else if (currentDate.equals(currentDate))
-            System.out.println("Ваша дата равна текущей.");
+        Calendar currentDate = new GregorianCalendar();                 //создание новой текущей даты
+        currentDate.set(Calendar.HOUR_OF_DAY, 0);                       //присвоение полям времени нулей
+        currentDate.set(Calendar.MINUTE, 0);
+        currentDate.set(Calendar.SECOND, 0);
+        currentDate.set(Calendar.MILLISECOND, 0);
+        Calendar inputDate = new GregorianCalendar(arrayOfDate[2], arrayOfDate[1] - 1, arrayOfDate[0]); //создание даты пользователя
+        if (currentDate.equals(inputDate))                  //проверка на равенство
+            System.out.println("Ваша дата равна текущей."); //вывод о равенстве
+        else if (currentDate.before(inputDate))             //проверка на предшествие
+            System.out.println("Ваша дата позже текущей."); //вывод о предшествии
         else
-            System.out.println("Ваша дата раньше текущей.");
+            System.out.println("Ваша дата раньше текущей.");//вывод о последовании
     }
 
-    public static void formDate() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите дату:");
-        /*
-            Проверки:
-            20031125
-            0220
-
-            16700709
-            1435
-         */
-        String date = in.nextLine();
-        String time = in.nextLine();
-        int year = Integer.parseInt(date.substring(0, 4));
+    public static void formDate() {                 //метод создания объектов классов Date и Calendar
+        Scanner in = new Scanner(System.in);        //подключение считывания с консоли
+        System.out.println("Введите дату:");        //вывод о вводе необходимых данных
+        String date = in.nextLine();                //считывание даты
+        String time = in.nextLine();                //считывание времени
+        int year = Integer.parseInt(date.substring(0, 4));      //разбиение на год, месяц, день, час и минуту
         int month = Integer.parseInt(date.substring(4, 6));
         int day = Integer.parseInt(date.substring(6, 8));
         int hour = Integer.parseInt(time.substring(0, 2));
         int minutes = Integer.parseInt(time.substring(2, 4));
 
-        Date generateDate = new Date((day - 1) * 86400000L);    //установка числа т.к. начальная дата - 1 января
-        /*
-            Сейчас начнутся танцы с бубном.
-            Установка через миллисекунды это ужасно неудобно, я решил не делать.
-            Методы set устарели, тут неприятно(((.
-         */
-        generateDate.setYear(year - 1900);  //Придётся отменять нормализацию.
-        generateDate.setMonth(month - 1);   //месяца с нуля ыыы
-        generateDate.setHours(hour);
-        generateDate.setMinutes(minutes);
-        System.out.println(generateDate);
+        Date generateDate = new Date((day - 1) * 86400000L);    //создание объекта класса Date через миллисекунды
+        generateDate.setYear(year - 1900);  //установка года
+        generateDate.setMonth(month - 1);   //установка месяца
+        generateDate.setHours(hour);        //установка часа
+        generateDate.setMinutes(minutes);   //установка минуты
+        System.out.println(generateDate);   //вывод на экран generateDate
 
-        Calendar generateCalendar = new GregorianCalendar();
-        generateCalendar.set(year, month - 1, day, hour, minutes, 0);
-        System.out.println(generateCalendar.getTime());
+        Calendar generateCalendar = new GregorianCalendar();            //создание объекта класса Calendar
+        generateCalendar.set(year, month - 1, day, hour, minutes, 0);   //установка года, месяца, дня, часа, минуты и секунды
+        System.out.println(generateCalendar.getTime());                 //вывод на экран этого generateCalendar
     }
 
-    public static void checkLists() {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        long startDate, endDate, calculation;
+    public static void checkLists() {                           //метод проверки скорости работы коллекций
+        Integer[] arr = {10, 4, 5, 6, 3};
+        ArrayList<Integer> arrayList = new ArrayList<>();       //коллекция ArrayList
+        LinkedList<Integer> linkedList = new LinkedList<>();    //коллекция LinkedList
+        long startDate, endDate, calculation;                   //переменные для подсчёта времени
+
+        Collections.addAll(arrayList, arr);                     //вставка базовых значений
+        Collections.addAll(linkedList, arr);
 
         System.out.println("ArrayList:");
         startDate = System.nanoTime();
-        arrayList.add(1);
+        arrayList.add(1);                                           //добавление элемента
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время добавления нс: " + calculation);
         startDate = System.nanoTime();
-        arrayList.add(1, 2);
+        arrayList.add(1, 2);                                        //добавление по индексу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время вставки нс: " + calculation);
         startDate = System.nanoTime();
-        arrayList.contains(1);
+        arrayList.contains(5);                                      //поиск по образцу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время поиска нс: " + calculation);
         startDate = System.nanoTime();
-        arrayList.remove(1);
+        arrayList.remove(2);                                        //удаление элемента по индексу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
-        System.out.println("Время вставки нс: " + calculation);
+        System.out.println("Время удаления нс: " + calculation);
 
         System.out.println("LinkedList:");
         startDate = System.nanoTime();
-        linkedList.add(1);
+        linkedList.add(1);                                          //добавление элемента
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время добавления нс: " + calculation);
         startDate = System.nanoTime();
-        linkedList.add(1, 2);
+        linkedList.add(1, 2);                                       //добавление по индексу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время вставки нс: " + calculation);
         startDate = System.nanoTime();
-        linkedList.contains(1);
+        linkedList.contains(5);                                     //поиск по образцу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
         System.out.println("Время поиска нс: " + calculation);
         startDate = System.nanoTime();
-        linkedList.remove(1);
+        linkedList.remove(2);                                       //удаление элемента по индексу
         endDate = System.nanoTime();
         calculation = endDate - startDate;
-        System.out.println("Время вставки нс: " + calculation);
+        System.out.println("Время удаления нс: " + calculation);
     }
 
     public static void main(String[] args) {
-        printEmployee("Вадим Малин");   //ex1
+        printEmployee("Вадим Малин");   //Ex1
         System.out.println();
 
         equalsDate();   //Ex2
