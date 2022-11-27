d24a3994ff3ec2c61a38144f200f72e2ddfe78ea
Marenau <malin.vadimka@mail.ru>
2022-11-27 23:04:02 +0300

    Add comments

--- mirea/lab/lab12/Manipulator.java
+++ mirea/lab/lab12/Manipulator.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class Manipulator {
     public static void main(String[] arg) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write the text");
-        String text = scanner.nextLine();
-        System.out.println("Write the pattern");
-        String patter = scanner.nextLine();
-        text = Arrays.toString(text.split(patter));
-        System.out.println("Divided text:");
-        System.out.println(text);
+        Scanner scanner = new Scanner(System.in);   //подключение сканера
+        System.out.println("Write the text");       //вывод о необходимости ввода
+        String text = scanner.nextLine();           //считывание строки с консоли
+        System.out.println("Write the pattern");    //вывод о необходимости ввода
+        String patter = scanner.nextLine();         //считывание строки с консоли
+        text = Arrays.toString(text.split(patter)); //разделение строки
+        System.out.println("Divided text:");        //вывод о разбиении
+        System.out.println(text);                   //вывод разбитого текста
     }
 }
