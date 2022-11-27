1dbb77b84e7a4abcd81a1c1d55bb6c72254de1fe
Marenau <malin.vadimka@mail.ru>
2022-11-27 22:32:19 +0300

    Add comments

--- mirea/lab/lab11/Test.java
+++ mirea/lab/lab11/Test.java
@@ -6,13 +6,13 @@ import java.util.HashSet;
 
 public class Test {
     public static void main(String[] arg) {
-        Solution solution = new Solution();
-        String[] arr = new String[]{"Hello", "world", "!"};
-        ArrayList<String> arrayList = solution.newArrayList(arr);
-        System.out.println(arrayList);
-        HashSet<String> hashSet = solution.newHashSet(arr);
-        System.out.println(hashSet);
-        HashMap<String, String> hashMap = solution.newHashMap(arr, new String[]{"H", "w", "!"});
-        System.out.println(hashMap);
+        Solution solution = new Solution();                         //создание объекта класса Solution
+        String[] arr = new String[]{"Hello", "world", "!"};         //создание массива строк
+        ArrayList<String> arrayList = solution.newArrayList(arr);   //инициализация списка типа ArrayList значением метода newArrayList
+        System.out.println(arrayList);                              //вывод списка
+        HashSet<String> hashSet = solution.newHashSet(arr);         //инициализация списка типа HashSet значением метода newHashSet
+        System.out.println(hashSet);                                //вывод списка
+        HashMap<String, String> hashMap = solution.newHashMap(arr, new String[]{"H", "w", "!"});    //инициализация списка типа HashMap значением метода newHashMap
+        System.out.println(hashMap);                                //вывод списка
     }
 }
